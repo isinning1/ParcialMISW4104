c644a837f20ce5f77b8e1b07534e8b814cff62b3
Irina Sinning <i.sinning@uniandes.edu.co>
2023-11-05 11:27:03 -0500

    Se define la función http get en el servicio

--- src/app/planta/planta.service.ts
+++ src/app/planta/planta.service.ts
@@ -1,11 +1,21 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 
+import { environment } from 'src/environments/environment';
+import { Observable } from 'rxjs';
+import { Planta } from './planta';
+
 @Injectable({
   providedIn: 'root'
 })
 export class PlantaService {
 
-  constructor(private http: HttpClient) { }
+  private apiUrl: string = environment.baseUrl + 'plantas';
 
-}
+  constructor(private http: HttpClient) { }
+ 
+  getPlantas(): Observable<Planta[]> {
+    return this.http.get<Planta[]>(this.apiUrl);
+  }
+ 
+ }
